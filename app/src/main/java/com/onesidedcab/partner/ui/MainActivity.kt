2edfda305bc22a8a18731f33d92db33cc197597d
dh23mobiledev <dh23mobiledev@gmail.com>
2018-06-07 11:52:08 +0530

    Initial commit

---
+++ app/src/main/java/com/onesidedcab/partner/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.onesidedcab.partner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics());
+        setContentView(R.layout.activity_main)
+
+    }
+}
